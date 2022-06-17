718e53c0b18946c9c325c3c756dd9dc47234b5c0
Stanislav Polivtsev <stas64a@gmail.com>
2022-06-17 19:55:24 +0000

    Current filter safe

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,8 @@
 from django import template
 from django.contrib.auth import get_user_model
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
 
 user_model = get_user_model()
 
@@ -12,8 +15,17 @@ def author_details(author):
         return ""
 
     if author.first_name and author.last_name:
-        name = f"{author.first_name} {author.last_name}"
+        name = escape(f'{author.first_name} {author.last_name}')
     else:
-        name = f"{author.username}"
+        name = escape(f'{author.username}')
+    
+    name = mark_safe(name)
 
+    if author.email:
+        # Escape email address
+        email = escape(author.email)
+        # Mark safe generated link
+        r_link = mark_safe(f'<a href="mailto:{email}">{name}</a>')
+        return r_link
+    
     return name
