772487c491227000417ecc99d2fb61e2cd4ef1f4
Stanislav Polivtsev <stas64a@gmail.com>
2022-06-17 19:17:34 +0000

    Current Filters

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
+
+def page1(request):
     return render(request, "blog/index.html")
 
 def page2(request):
