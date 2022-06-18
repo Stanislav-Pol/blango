04c08f9745756b4e07b78183df05280863e8f46d
Stanislav Polivtsev <stas64a@gmail.com>
2022-06-18 08:39:30 +0000

    Inclusion Tag

--- blango/urls.py
+++ blango/urls.py
@@ -20,6 +20,7 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.index),
-    path('index.html', blog.views.index),
+    path('index.html', blog.views.index, name="index"),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path('page2.html', blog.views.page2),
 ]
