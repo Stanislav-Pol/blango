3a805eaa8de12732a18ddf5e5eaf3e7a2e68dd9d
Stanislav Polivtsev <stas64a@gmail.com>
2022-07-24 13:36:21 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -53,6 +53,7 @@ class Dev(Configuration):
         'allauth.account', 
         'allauth.socialaccount', 
         'allauth.socialaccount.providers.google',
+        'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -66,6 +67,17 @@ class Dev(Configuration):
         #'django.middleware.clickjacking.XFrameOptionsMiddleware',
         
     ]
+    
+    # Add Token Authentication
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
+    
+    
     #Allauth settings (W3.3)
     SITE_ID = 1
     ACCOUNT_USER_MODEL_USERNAME_FIELD = None
--- blog/api/views.py
+++ blog/api/views.py
@@ -3,12 +3,19 @@ from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    #List of the allowed authentication methods
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    #List of the allowed authentication methods
+    #authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    authentication_classes = [SessionAuthentication,]
